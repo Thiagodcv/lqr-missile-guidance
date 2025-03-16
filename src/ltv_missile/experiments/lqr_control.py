46297b618ffcea5775416d64aebad1f07d64672f
Thiago da Cunha Vasco <thiagomonnerat2000@gmail.com>
2025-03-16 11:19:47 -0700

    Implemented sdeint for LTV lqr system

--- src/ltv_missile/experiments/lqr_control.py
+++ src/ltv_missile/experiments/lqr_control.py
@@ -5,6 +5,7 @@ from src.ltv_missile.lqr import A_nom, B_nom, get_S_interp, S
 from src.ltv_missile.nom_traj import nom_traj_params, nom_state
 import constants as const
 from src.utils import plot_dynamics
+import sdeint
 
 
 def experiment():
@@ -45,35 +46,37 @@ def experiment():
     # Get S matrix estimate based on cubic splines
     S_interp = get_S_interp(Q, Q, R, fe_nom, th_nom, bc['T'])
 
-    def opt_u(t, x):
+    def opt_u(x, t):
         # Compute LQR control input
         K = R_inv @ B_nom(t, fe_nom, th_nom).T @ S(t, S_interp, n)
         dx = x - nom_state(t, fe_nom, th_nom, bc)
         u = -K @ dx + nom_input
         return u
 
-    def dyn(t, x):
+    def dyn(x, t):
         # Closed-loop system
-        u = opt_u(t, x)
+        u = opt_u(x, t)
         x_dot = f(x, u)
-
-        # Add noise to theta_ddot
-        x_dot[5] += np.random.normal(scale=0.1)
         return x_dot
+
+    G_mat = np.zeros((7, 7))
+    G_mat[5, 5] = 0.1
+
+    def G(x, t):
+        return G_mat
 
     th0 = np.pi/8
     m0 = const.MASS
     init_state = np.array([0., 0., 0., 0., th0, 0., m0])
-
-    sol = solve_ivp(dyn, [0., bc['T']], init_state, rtol=1e-3, atol=1e-6)
+    t_span = np.linspace(0.0, bc['T'], 3000)
+    sol = sdeint.itoint(dyn, G, init_state, t_span)
     print(sol)
 
     # Plot results
-    t_seq = sol.t
-    x_seq = [sol.y[:, t] for t in range(sol.y.shape[1])]
-    nom_state_seq = np.array([nom_state(t, fe_nom, th_nom, bc) for t in t_seq]).T
-    true_input_seq = np.array([opt_u(t, x) for t, x in zip(t_seq, x_seq)]).T
-    plot_dynamics(t_seq, sol, nom_state_seq, true_input_seq, fe_nom, fe_lim=[4600, 4750])
+    x_seq = [sol[t, :] for t in range(sol.shape[0])]
+    nom_state_seq = np.array([nom_state(t, fe_nom, th_nom, bc) for t in t_span])
+    true_input_seq = np.array([opt_u(x, t) for x, t in zip(x_seq, t_span)])
+    plot_dynamics(t_span, sol, nom_state_seq, true_input_seq, fe_nom, fe_lim=[4600, 4750])
 
 
 if __name__ == '__main__':
