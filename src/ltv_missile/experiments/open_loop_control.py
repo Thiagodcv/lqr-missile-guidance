eea3b06afd242f495b6a76a39067f1a7566c62f3
Thiago da Cunha Vasco <thiagomonnerat2000@gmail.com>
2025-03-16 11:23:05 -0700

    Implemented sdeint into LTV open loop control

--- src/ltv_missile/experiments/open_loop_control.py
+++ src/ltv_missile/experiments/open_loop_control.py
@@ -5,6 +5,7 @@ from src.ltv_missile.lqr import A_nom, B_nom, get_S_interp, S
 from src.ltv_missile.nom_traj import nom_traj_params, nom_state
 import constants as const
 from src.utils import plot_dynamics
+import sdeint
 
 
 def experiment():
@@ -24,27 +25,29 @@ def experiment():
     print("th_nom: ", th_nom)
     # print(nom_state(5, fe_nom, th_nom, bc))
 
-    def dyn(t, x):
+    def dyn(x, t):
         # Closed-loop system
         u = nom_input
         x_dot = f(x, u)
-
-        # Add noise to theta_ddot
-        x_dot[5] += np.random.normal(scale=0.1)
         return x_dot
+
+    G_mat = np.zeros((7, 7))
+    G_mat[5, 5] = 0.1
+
+    def G(x, t):
+        return G_mat
 
     th0 = th_nom
     m0 = const.MASS
     init_state = np.array([0., 0., 0., 0., th0, 0., m0])
-
-    sol = solve_ivp(dyn, [0., bc['T']], init_state, rtol=1e-3, atol=1e-6)
+    t_span = np.linspace(0.0, bc['T'], 3000)
+    sol = sdeint.itoint(dyn, G, init_state, t_span)
     print(sol)
 
     # Plot results
-    t_seq = sol.t
-    nom_state_seq = np.array([nom_state(t, fe_nom, th_nom, bc) for t in t_seq]).T
-    true_input_seq = np.array([nom_input for t in t_seq]).T
-    plot_dynamics(t_seq, sol, nom_state_seq, true_input_seq, fe_nom, fe_lim=[4600, 4750])
+    nom_state_seq = np.array([nom_state(t, fe_nom, th_nom, bc) for t in t_span])
+    true_input_seq = np.array([nom_input for t in t_span])
+    plot_dynamics(t_span, sol, nom_state_seq, true_input_seq, fe_nom, fe_lim=[4600, 4750])
 
 
 if __name__ == '__main__':
