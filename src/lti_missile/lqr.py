5bfe1978b85974f9465d13ec05095e1c90d2678d
Thiago da Cunha Vasco <thiagomonnerat2000@gmail.com>
2025-03-24 11:29:22 -0700

    Fixed documentation

--- src/lti_missile/lqr.py
+++ src/lti_missile/lqr.py
@@ -4,6 +4,20 @@ from scipy.linalg import solve_continuous_are
 
 
 def A_nom(fe, th):
+    """
+    The time-independent A matrix for the linearized model.
+
+    Parameters:
+    ----------
+    fe: float
+        The thrust exerted by the missile under the nominal trajectory.
+    th: float
+        The pitch angle of the missile under the nominal trajectory.
+
+    Returns:
+    -------
+    ndarray
+    """
     m = const.MASS
     A = np.array([[0., 1., 0., 0., 0., 0.],
                   [0., 0., 0., 0., fe/m*np.cos(th), 0.],
@@ -15,6 +29,20 @@ def A_nom(fe, th):
 
 
 def B_nom(fe, th):
+    """
+    The time-independent B matrix for the linearized model.
+
+    Parameters:
+    ----------
+    fe: float
+        The thrust exerted by the missile under the nominal trajectory.
+    th: float
+        The pitch angle of the missile under the nominal trajectory.
+
+    Returns:
+    -------
+    ndarray
+    """
     l1 = const.L1
     l2 = const.L2
     ln = const.Ln
@@ -31,6 +59,25 @@ def B_nom(fe, th):
 
 
 def S(Q, R, fe, th):
+    """
+    Compute the S matrix which solves the Algebraic Riccati Equation for infinite-horizon
+    LQR.
+
+    Parameters:
+    ----------
+    Q: ndarray
+        The state cost within the LQR objective.
+    R: ndarray
+        The input cost within the LQR objective.
+    fe: float
+        The thrust exerted by the missile under the nominal trajectory.
+    th: float
+        The pitch angle of the missile under the nominal trajectory.
+
+    Returns:
+    -------
+    ndarray
+    """
     A = A_nom(fe, th)
     B = B_nom(fe, th)
     return solve_continuous_are(A, B, Q, R)
